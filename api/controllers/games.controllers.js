ffbfda95898ad430ced0ee3988c017eaf2d14f6d
oscar-gordillo <112912606+oscar-gordillo@users.noreply.github.com>
2022-11-03 21:02:32 -0500

    day 5

--- api/controllers/games.controllers.js
+++ api/controllers/games.controllers.js
@@ -8,12 +8,15 @@ module.exports.getAll= function(req, res) {
 
     const gameCollection= db.collection("games");
     let offset= 0;
-    let count= 5;
+    let count= 4;
     if (req.query && req.query.offset) {
-    offset= parseInt(req.query.offset, 10);
+        offset= parseInt(req.query.offset, 10);
     }
     if (req.query && req.query.count) {
-    count= parseInt(req.query.count, 10);
+        count= parseInt(req.query.count, 10);
+        if (count>7) {
+            count=7;
+        }
     }
     gameCollection.find().skip(offset).limit(count).toArray(function(err,games) {
         console.log("Found games", games);
@@ -33,6 +36,16 @@ module.exports.getOne= function(req, res) {
         });
     }
 
+    module.exports.deleteOne= function(req, res) {
+        const db= dbConnection.get();
+        const gamesCollection= db.collection("games");
+        const gameId= req.params.gameId;
+        gamesCollection.deleteOne({_id : ObjectId(gameId)}, function(err,deletedCount) {
+            console.log("deleted game", deletedCount);
+            res.status(200).json(deletedCount);
+            });
+        }
+
     module.exports.addOne= function(req, res) {
         const db= dbConnection.get();
         const gamesCollection= db.collection("games");
@@ -40,15 +53,30 @@ module.exports.getOne= function(req, res) {
         if (req.body && req.body.title&& req.body.price) {
             newGame.title= req.body.title;
             newGame.price= parseFloat(req.body.price);
-            gamesCollection.insertOne(newGame, function(err, response) {
+            let minPlayers=req.body.minPlayers;
+            let minAge= req.body.minAge;
+            newGame.minPlayers=minPlayers;
+            newGame.minAge=minAge;
+            if (minPlayers<1 || minPlayers > 11) {
+                res.status(400).send('min players must be between 1 and 11');
+                return;
+            }
+            if (minAge<6 || minAge > 99) {
+                res.status(400).send('min age must be between 6 and 99');
+                return;
+            }
+            gamesCollection.insertOne(newGame, function(err, insertedId) {
                 if (err) {
                     res.status(500).json({error: err});
                 } else {
                     console.log(response);
-                    res.status(201).json(response);
+                    res.status(201).json(insertedId);
                 }
         });
 
+    }else{
+        res.status(400).send('title and price are required');
+        return;
     }
 }        
     
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ const gamesController= require("../controllers/games.controllers");
 
 router.route("/games").get(gamesController.getAll).post(gamesController.addOne);
 
-router.route("/games/:gameId").get(gamesController.getOne);
+router.route("/games/:gameId").get(gamesController.getOne).delete(gamesController.deleteOne);
 
 router.route("/json")
 .get(function(req, res) {
